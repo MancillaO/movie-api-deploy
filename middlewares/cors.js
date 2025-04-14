cb1eb669eb9d3a6e22fcf622a462c99059630f8f
MancillaO <edgarmncllav04@gmail.com>
2025-04-13 18:44:52 -0600

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,7 @@ export const corsMiddleware = () => cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
       'http://localhost:8080',
-      'https://movies.com'
+      'https://movie-api-3ifg.onrender.com'
     ]
     if (ACCEPTED_ORIGINS.includes(origin)) {
       return callback(null, true)
